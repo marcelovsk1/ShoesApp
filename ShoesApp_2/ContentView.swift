bbe8b13a330d019c32c2db4052db4838f220c851
Marcelo Amaral Alves <marceloaa@live.com>
2024-05-01 16:06:30 -0300

    custom scene view

---
+++ ShoesApp_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoesApp_2
+//
+//  Created by Marcelo Amaral Alves on 2024-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoesApp_2/ShoesApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoesApp_2App.swift
+//  ShoesApp_2
+//
+//  Created by Marcelo Amaral Alves on 2024-05-01.
+//
+
+import SwiftUI
+
+@main
+struct ShoesApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
